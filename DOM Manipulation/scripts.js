78c32ba1304427bb8e501db9b982b784a77f14da
kellyo-smith <smi22002@byui.edu>
2022-05-17 11:27:21 -0500

    color fix logo

---
+++ DOM Manipulation/scripts.js
@@ -0,0 +1,7 @@
+const button = document.getElementsByName(button);
+const input = document.querySelector("#input");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", function {
+																								
+														})
