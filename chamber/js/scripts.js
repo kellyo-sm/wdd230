d069cf06ebc8e88629a91dbfc9a1fdbd96adb499
kellyo-smith <smi22002@byui.edu>
2022-05-20 08:59:34 -0500

    banner not display   heroimg not responsive

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -18,7 +18,10 @@ let d = new Date().getDay();
 const banner = document.getElementById("banner");
 if (d === 1 || d === 2) {
   banner.style.display = "block";
-} 
+  
+} else {
+  banner.style.display = "none";
+}
 
 const close = document.querySelector("#close");
 
