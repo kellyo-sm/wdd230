b9be98342f06492c87b4c6a8f1936a9d04cb727a
kellyo-smith <smi22002@byui.edu>
2022-05-21 09:05:50 -0500

    week05 submission

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -17,8 +17,11 @@ datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 let d = new Date().getDay();
 const banner = document.getElementById("banner");
 if (d === 1 || d === 2) {
-  banner.style.display = "block";
-  
+  banner.style.display = "block"; 
+} 
+
+if (d === 1 || d === 2) {
+  banner.style.display = "block"; 
 } else {
   banner.style.display = "none";
 }
