93db8586e3d07b1c7f08ef5b13cf7e70e3d89f4e
kellyo-smith <smi22002@byui.edu>
2022-06-10 12:51:02 -0500

    join page

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -59,3 +59,14 @@ document.getElementById("currentdate").innerHTML = currentdate;
 
 document.write("Last Modified: " + document.lastModified);
 
+// hidden date and time
+
+var date = new Date();
+
+document.getElementById("date").time = date.toDateString();
+
+var hours = date.getHours();
+var mins = date.getMinutes();
+var seconds = date.getSeconds();
+document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
+
