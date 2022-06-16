01043455078b537088ca6bc0b849377bcf4972aa
kellyo-smith <smi22002@byui.edu>
2022-06-16 12:28:55 -0500

    directory javascript not working

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,4 +1,5 @@
-const requestURL = "json/data.json";
+const requestURL = "https://github.com/kellyo-sm/wdd230/blob/main/chamber/json/data.json";
+const cards = document.querySelector(".cards");
 
 fetch(requestURL)
     .then(function (response) {
@@ -8,11 +9,11 @@ fetch(requestURL)
         console.table(jsonObject);
 
         const directory = jsonObject["directory"];
-        const groups = document.querySelector(".groups");
+        
 
-        directory.forEach(company => {
+        directory.forEach(business => {
             let image = document.createElement("img");
-            let group = document.createElement("div");
+            let card = document.createElement("div");
             let businessName = document.createElement("h2");
             let address = document.createElement("p");
             let phone = document.createElement("p");
@@ -24,14 +25,37 @@ fetch(requestURL)
             image.setAttribute("alt", `${business.businessName}`);
             image.setAttribute("loading", "lazy");
 
-            group.appendChild(businessName);
-            group.appendChild(image);
-            group.appendChild(url);
-            group.appendChild(address);
-            group.appendChild(phone);
-            group.appendChild(membership);
+            card.appendChild(businessName);
+            card.appendChild(image);
+            card.appendChild(url);
+            card.appendChild(address);
+            card.appendChild(phone);
+            card.appendChild(membership);
 
-            groups.append(group);
+            // cards.append(card);
+            document.querySelector(cards).appendChild(card)
             
         });
+
+        directory.forEach(displayCards);
+
     });
+
+
+
+const gridbtn = document.querySelector("#grid");
+const listbtn = document.querySelector("#list");
+const display = document.querySelector(".cards");
+
+
+gridbtn.addEventListener("click", () => {
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbtn.addEventListener("click", showList);
+
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}
