df1acfd3f71c8b6f94942026f197dd029bb29934
kellyo-smith <smi22002@byui.edu>
2022-06-23 13:38:42 -0500

    chamber wireframes

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -59,17 +59,21 @@ function displayBusiness(business) {
 
 // grid and list
 
-getAffiliates()
-
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
 
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
 gridbutton.addEventListener("click", () => {
-	cards.classList.add("grids");
-	cards.classList.remove("lists");
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
 });
 
-listbutton.addEventListener("click", () => {
-	cards.classList.add("lists");
-	cards.classList.remove("grids");
-});
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
