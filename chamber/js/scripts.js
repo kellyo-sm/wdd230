6653caaca96cd2301265640d7a0d3ead258452ef
kellyo-smith <smi22002@byui.edu>
2022-06-30 10:18:04 -0500

    last minute changes 01

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -48,17 +48,17 @@ x.onclick = toggleMenu;
 
 
 
-// footer date/time/last updated
-var date = (new Date().getFullYear());
-document.getElementById("year").innerHTML = date;
+// // footer date/time/last updated
+// var date = (new Date().getFullYear());
+// document.getElementById("year").innerHTML = date;
 
 
-var currentdate = new Date().toLocaleDateString('en-GB', {weekday:"long", year:"numeric", month:"long", day:"numeric"});
-document.getElementById("currentdate").innerHTML = currentdate;
-document.currentdate.style.fontSize = "0.7em";
+// var currentdate = new Date().toLocaleDateString('en-GB', {weekday:"long", year:"numeric", month:"long", day:"numeric"});
+// document.getElementById("currentdate").innerHTML = currentdate;
 
 
-document.write("Last Modified: " + document.lastModified);
+
+// document.write("Last Modified: " + document.lastModified);
 
 
 
