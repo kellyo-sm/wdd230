bb352311afec3189c520865cd1820729f5aecef5
kellyo-smith <smi22002@byui.edu>
2022-07-20 17:43:27 -0500

    well i got the current temp working at least

--- templeinn/js/weather.js
+++ templeinn/js/weather.js
@@ -1,5 +1,5 @@
 const currentOutlook = document.querySelector('#current');
-const weatherIcon = document.querySelector('.weather-icon');
+const weatherIcon = document.querySelector('#weather-icon');
 const temperature = document.querySelector('#temp');
 const humidity = document.querySelector('#humidity');
 const captionDesc = document.querySelector('figcaption');
@@ -12,12 +12,9 @@ const day3 = document.querySelector('#day3');
 const alertText = document.querySelector('.eText');
 
 
-const latitude = 38.981
-const longitude = -77.100
+const url = `https://api.openweathermap.org/data/2.5/onecall?lat=38.9807&lon=-77.1003&units=imperial&exclude=minutely,hourly&appid=4c30c204135027cef9944117fb3a6fbc`;
 
-const url = `https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&appid=4c30c204135027cef9944117fb3a6fbc`;
-
-// apiFetch(url);
+apiFetch(url);
 
 async function apiFetch(apiURL) {
   try {
@@ -33,53 +30,54 @@ async function apiFetch(apiURL) {
   }
 }
 
-function displayResults (weather) {
-  currentOutlook.innerHTML = `<strong>${weather.current.temp.toFixed(0)} &#8457;</strong>`;
+function displayResults(weatherData) {
 
-  const iconsrc =  `https://openweathermap.org/img/w/${weather.current.weather[0].icon}.png`;
-  const desc = toTitleCase(weather.current.weather[0].description);
+  currentOutlook.innerHTML = `<strong>${weatherData.current.temp.toFixed(0)} &#8457;</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = toTitleCase(weatherData.weather[0].description);
 
   weatherIcon.setAttribute('src', iconsrc);
   weatherIcon.setAttribute('alt', desc);
-  captionDesc.textContent = desc;
+  
 
-  currentHumidity.innerHTML = `Current Humidity: <strong>${weather.current.humidity}</strong>`;
+  humidity.innerHTML = `${weatherData.current.humidity}`;
 
-  const d1Img1 = `https://openweathermap.org/img/w/${weather.daily[0].weather[0].icon}.png`;
-  const d1Desc = weather.daily[0].weather[0].description;
+  const d1Img = `https://openweathermap.org/img/w/${weatherData.daily[0].weather[0].icon}.png`;
+  const d1Desc = weatherData.daily[0].weather[0].description;
 
-  img1.setAttribute('src', d1Img1);
+  img1.setAttribute('src', d1Img);
   img1.setAttribute('alt', d1Desc);
 
-  day1.innerHTML = `Temp: <strong>${weather.daily[0].temp.day}</strong> | Humidity: <strong>${weather.daily[0].humidity}</strong>`;
+  day1.innerHTML = `<strong>Temp:</strong> ${weatherData.daily[0].temp.day} | <strong>Humidity: </strong>${weatherData.daily[0].humidity}`;
 
-  const d2Img = `https://openweathermap.org/img/w/${weather.daily[1].weather[0].icon}.png`;
-  const d2Desc = weather.daily[1].weather[0].description;
+  const d2Img = `https://openweathermap.org/img/w/${weatherData.daily[1].weather[0].icon}.png`;
+  const d2Desc = weatherData.daily[1].weather[0].description;
 
   img2.setAttribute('src', d2Img);
   img2.setAttribute('alt', d2Desc);
 
-  day2.innerHTML = `Temp: <strong>${weather.daily[1].temp.day}</strong> | Humidity: <strong>${weather.daily[1].humidity}</strong>`;
+  day2.innerHTML = `<strong>Temp:</strong> ${weatherData.daily[1].temp.day} | <strongHumidity:/strong> >${weatherData.daily[1].humidity}<`;
 
-  const d3Img = `https://openweathermap.org/img/w/${weather.daily[2].weather[0].icon}.png`;
-  const d3Desc = weather.daily[2].weather[0].description;
+  const d3Img = `https://openweathermap.org/img/w/${weatherData.daily[2].weather[0].icon}.png`;
+  const d3Desc = weatherData.daily[2].weather[0].description;
 
   img3.setAttribute('src', d3Img);
   img3.setAttribute('alt', d3Desc);
 
-  day3.innerHTML = `Temp: <strong>${weather.daily[2].temp.day}</strong> | Humidity: <strong>${weather.daily[2].humidity}</strong>`;
+  day3.innerHTML = `<strong>Temp: </strong>${weatherData.daily[2].temp.day} | <strong>Humidity: </strong>${weatherData.daily[2].humidity}`;
 
   try {
-    let uTime = weather.alerts[0].end;
-    let date = new Date(uTime * 1000);
+    let localTime = weather.alerts[0].end;
+    let date = new Date(localTime * 1000);
 
     const hours = date.getHours();
     const minutes = "0" + date.getMinutes();
     const seconds = "0" + date.getSeconds();
 
-    const formattedTime = hours + ":" + minutes.substring(-2) + ":" + seconds.substring(-2);
+    const time = hours + ":" + minutes.substring(-2) + ":" + seconds.substring(-2);
 
-    alertText.innerHTML = `There is a current ${weather.alerts[0].event} in this area until ${formattedTime}`
+    alertText.innerHTML = `Issued for this location:  ${weather.alerts[0].event}  until ${time}`
     } catch (error) {
         document.querySelector(".ebroad").classList.add("closed");
     }
